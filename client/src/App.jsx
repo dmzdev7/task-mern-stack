1138b9faa7ef3add498fc9e05ae250bbcf1c1d2d
Baxter <davidf.martinez.br@gmail.com>
2023-12-06 16:07:05 -0500

    Added login & register pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<h1>Home</h1>}></Route>
-				<Route path="/login" element={<h1>Login</h1>}></Route>
-				<Route path="/register" element={<h1>Register</h1>}></Route>
+				<Route path="/login" element={<LoginPage />}></Route>
+				<Route path="/register" element={<RegisterPage />}></Route>
 				<Route path="/tasks" element={<h1>Tasks</h1>}></Route>
 				<Route path="/add-task" element={<h1>New Task</h1>}></Route>
 				<Route path="/tasks/:id" element={<h1>Update Task</h1>}></Route>
