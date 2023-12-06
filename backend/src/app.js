80b3498b5547be97da9430277136d89bb8df6e3c
Baxter <davidf.martinez.br@gmail.com>
2023-12-06 15:49:46 -0500

    Import cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
 import tasksRoutes from './routes/tasks.routes.js';
@@ -8,7 +9,15 @@ const app = express();
 
 //* Rastreo de peticiones
 app.use(morgan('dev'));
+app.use(
+	cors({
+		origin: 'http://localhost:5173',
+	})
+);
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(cookieParser());
 
 //* Rutas
