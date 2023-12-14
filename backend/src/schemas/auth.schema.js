0bd2546347fbd8d7201474431de87e1514a04d9f
Baxter <davidf.martinez.br@gmail.com>
2023-12-14 17:36:59 -0500

    Added messages for errors

--- backend/src/schemas/auth.schema.js
+++ backend/src/schemas/auth.schema.js
@@ -5,11 +5,19 @@ export const registerSchema = z.object({
 		required_error: 'Username is required',
 	}),
 	email: z
-		.string({ required_error: 'Email is required' })
-		.email({ message: 'Invalid email' }),
+		.string({
+			required_error: 'Email is required',
+		})
+		.email({
+			message: 'Email is not valid',
+		}),
 	password: z
-		.string({ required_error: 'Password is required' })
-		.min(8, { message: 'Password must be at least 8 characters' }),
+		.string({
+			required_error: 'Password is required',
+		})
+		.min(8, {
+			message: 'Password must be at least 8 characters',
+		}),
 });
 
 export const loginSchema = z.object({
