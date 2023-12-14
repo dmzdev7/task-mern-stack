9c8f75c4a4c2f549f40cf973e4f97ae54a01b37a
Baxter <davidf.martinez.br@gmail.com>
2023-12-14 15:44:50 -0500

    Added auth context to the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import { AuthProvider } from './context/AuthContext';
 
 const App = () => {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<h1>Home</h1>}></Route>
-				<Route path="/login" element={<LoginPage />}></Route>
-				<Route path="/register" element={<RegisterPage />}></Route>
-				<Route path="/tasks" element={<h1>Tasks</h1>}></Route>
-				<Route path="/add-task" element={<h1>New Task</h1>}></Route>
-				<Route path="/tasks/:id" element={<h1>Update Task</h1>}></Route>
-				<Route path="/profile" element={<h1>Profile</h1>}></Route>
-			</Routes>
-		</BrowserRouter>
+		<AuthProvider>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<h1>Home</h1>}></Route>
+					<Route path="/login" element={<LoginPage />}></Route>
+					<Route path="/register" element={<RegisterPage />}></Route>
+					<Route path="/tasks" element={<h1>Tasks</h1>}></Route>
+					<Route path="/add-task" element={<h1>New Task</h1>}></Route>
+					<Route path="/tasks/:id" element={<h1>Update Task</h1>}></Route>
+					<Route path="/profile" element={<h1>Profile</h1>}></Route>
+				</Routes>
+			</BrowserRouter>
+		</AuthProvider>
 	);
 };
 
