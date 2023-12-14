dead610c9b92f73a5ca198aef20341f563ed2b8f
Baxter <davidf.martinez.br@gmail.com>
2023-12-14 17:55:14 -0500

    Added handle error & auth context

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,30 +1,39 @@
 import { createContext, useState, useContext } from 'react';
 import { registerRequest } from '../api/auth';
 
-export const AuthContext = createContext();
+const AuthContext = createContext();
 
 export const useAuth = () => {
 	const context = useContext(AuthContext);
-	if (!context) {
-		throw new Error('useAuth must be used within a AuthProvider');
-	}
+	if (!context) throw new Error('useAuth must be used within a AuthProvider');
 	return context;
 };
 
 export const AuthProvider = ({ children }) => {
 	const [user, setUser] = useState(null);
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const [errors, setErrors] = useState([]);
 
 	const signup = async (user) => {
-		const res = await registerRequest(user);
-		console.log(res.data);
-		setUser(res.data);
+		try {
+			const res = await registerRequest(user);
+			if (res.status === 200) {
+				setUser(res.data);
+				setIsAuthenticated(true);
+			}
+		} catch (error) {
+			console.log(error.response.data);
+			setErrors(error.response.data.message);
+		}
 	};
 
 	return (
 		<AuthContext.Provider
 			value={{
-				signup,
 				user,
+				signup,
+				isAuthenticated,
+				errors,
 			}}
 		>
 			{children}
